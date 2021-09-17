5e78b127e442a46f715f1c25d7fd5b1b01232785
GreenVan72 <67523558+GreenVan72@users.noreply.github.com>
2021-09-17 09:59:16 +0200

    poem

---
+++ PYTB1L2PoemPrinting.py
@@ -0,0 +1,8 @@
+vers1 = "ik ben Erdem,"
+vers2 = "ik woon in zaandam,"
+vers3 = "ik vind gamen en tekenen leuk,"
+vers4 = "ik speel veel rpg en fighting games,"
+vers5 = "in mijn vrije tijd game ik lol."
+
+
+print(vers1, vers2, vers3, vers4, vers5)
