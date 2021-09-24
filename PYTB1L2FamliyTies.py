3886dce5800082e1a1a5ec3a9694861d01cf3a96
GreenVan72 <67523558+GreenVan72@users.noreply.github.com>
2021-09-24 09:33:27 +0200

    idk info lol

---
+++ PYTB1L2FamliyTies.py
@@ -0,0 +1,10 @@
+import time
+import random
+import sys
+
+naam = input("geef uw naam aan")
+jaar_oud = input("geef uw leeftijd aan")
+woonplaats = input("geef uw woonplaats aan")
+km = input("hoeveel kilometer van school woont u?")
+
+print("Mijn naam is " + naam +", ik ben " + jaar_oud + " jaar oud en woon in " + woonplaats +". Ik woon " + km + " kilometer van het Mediacollege.")
