a9f1ec5a415a9a333ec36aec43ce7a8e1f82d3ac
GreenVan72 <67523558+GreenVan72@users.noreply.github.com>
2021-09-24 09:17:49 +0200

    pizza

---
+++ ditbenik.py
@@ -0,0 +1,28 @@
+import time
+import random
+import sys
+print ("Hey lol my name Jeff")
+print (" wat denk je dat mijn achternaam is?")
+d1 = input (": A) rey Bezos. B) jeff [A/B]: ")
+if d1 == "A":
+    print ("Wow! Zo goed! U bent echt goed in achternamen raden.")
+    print ("op naar het volgende vraag!!!!!!")
+if d1 == "B":
+    print ("wow. U bent egt dom lol")
+    print ("ok volgende vraag.")
+print("hoe oud denkt u dat ikki ben?")
+d2 = input ("; A) 69. B) 420. [A/B]: ")
+if d2 == "A":
+    print("haha funny number!")
+if d2 == "B":
+    print("haha funny number!")
+print("de het laatste vraag! Naar wat ruikt de kleur 9? ")
+d3 = input ("; A) groen B) 45 [A/B]: ")
+if d3 == "A":
+    print("fout")
+if d3 == "B":
+    print("fout")
+print("het antwoord was niks. U bent niet echt zo slim of wel?")
+print("thank you for coming to my ted talk.")
+
+        
---
+++ stel_vragen.py
@@ -0,0 +1,11 @@
+import time
+import random
+import sys
+
+antwoord1 = input("Hoe oud ben je? ")
+antwoord2 = input("waar woon je? ")
+antwoord3 = input("wat doe je graag? ")
+
+print(":0 omg, je bent dus " + antwoord1 + " jaar oud, zo cool!")
+print(":0 omg, je woont dus in " + antwoord2 + " ik ook!")
+print(":0 omg, ik doe " + antwoord3 + " ook graag!")
