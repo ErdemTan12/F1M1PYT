f473d22a805cac7ac8d3d32172c36b9824a63dca
GreenVan72 <67523558+GreenVan72@users.noreply.github.com>
2021-11-02 11:51:21 +0100

    callme

---
+++ PYTB1L5JustCallMe.py
@@ -0,0 +1,28 @@
+import sys
+import time
+import asyncio
+from asyncio import sleep
+
+name = input("what is yur name?")
+number = input("how much bobux ugot")
+
+print("&: Hey lol!")
+time.sleep(2)
+print("&: oh hey lol!")
+time.sleep(2)
+print("#: It's a me "+ name)
+time.sleep(2)
+print("&: hey "+ name+ " lol! i'm gay!")
+time.sleep(2)
+print("#: same!")
+time.sleep(2)
+print("&: How much bobux you got?")
+time.sleep(2)
+print("#: "+number)
+time.sleep(2)
+print("&: omg thats so cool!")
+time.sleep(2)
+print("#: gtg poop bye lol!")
+time.sleep(2)
+print("&: oh okay bye lol!")
+
