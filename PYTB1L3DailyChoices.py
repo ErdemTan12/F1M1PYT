b1b7ac315195f6da03d83380fe6446e35a14b27c
GreenVan72 <67523558+GreenVan72@users.noreply.github.com>
2021-11-02 11:29:22 +0100

    daily choices

---
+++ PYTB1L3DailyChoices.py
@@ -0,0 +1,15 @@
+print("Your alarm rings, do you GETUP, SNOOZE, EET PIZZA, POEPEN OF IJS ETEN ?")
+choice = input()
+if choice == 'GETUP':
+    print("You get out of bed and feel fresh!")
+elif choice == 'SNOOZE':
+    print("Hmmmmmm zzzzzzzzzzzzzzzzzzzzzzzzzz")
+elif choice == 'EET PIZZA':
+    print("Hmmmmmm pizzaaaaaaaaaaaaaaaaaaaaaaaaa")
+elif choice == 'POEPEN':
+    print("je hebt zo hard gepoept")
+elif choice == 'IJ ETEN':
+    print("Hmmmmmm ijs!")
+
+else:
+    print(choice, " kan niet >:(")
